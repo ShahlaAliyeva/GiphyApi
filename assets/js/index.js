f8b494ead6328cc07192d2078fcdb8c57d947bd5
ShahlaAliyeva <raufa5740@gmail.com>
2022-02-27 19:59:59 +0400

    bugs was fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
-// array of strings for my topics.
-var topics = ['Dog', 'Cat', 'Rabbit', 'Hamster', 'Skunk', 'Goldfish','Bird', 'Ferret', 'Turtle','Sugar Glider','Chinchilla' ,'Hedgehog','Hermit Grab','Gerbil','Chicken','Capybara','Teacup Pig','Serval','Salamander','Frog', 'Alligator'];
+
+var inputs = ['Dog', 'Cat', 'Rabbit', 'Hamster', 'Skunk', 'Goldfish', 'Bird', 'Ferret', 'Turtle', 'Sugar Glider', 'Chinchilla', 'Hedgehog', 'Hermit Grab', 'Gerbil', 'Chicken', 'Capybara', 'Teacup Pig', 'Serval', 'Salamander', 'Frog', 'Alligator'];
 var title;
 var animate;
 var still;
@@ -8,73 +8,73 @@ var state;
 function createButton(animalName){
   var input = $("<button type='button' class = 'button' value='" + animalName + "'/>");
   input.appendTo($('h1'));
-}*/ 
-function createButton(animalName){
-  var input = $("<input type='button' class = 'button' value='" + animalName + "'/>");
-  input.appendTo($('h1'));
+}*/
+function createButton(animalName) {
+    var input = $("<input type='button' class = 'button' value='" + animalName + "'/>");
+    input.appendTo($('h1'));
 }
 
-function renderButtons(){
-  $('h1').empty();
-  for (var i = 0; i < topics.length; i++) {
-  createButton(topics[i]);
-  }
+function addButtons() {
+    $('h1').empty();
+    for (var i = 0; i < inputs.length; i++) {
+        createButton(inputs[i]);
+    }
 }
 
-$(document).ready(function(){
-renderButtons();  
+$(document).ready(function () {
+    addButtons();
 
-$('#submit').click(function(){
-  var userAnimal = $('#userInput').val().trim();
-  topics.push(userAnimal);
-  renderButtons();
-})  
-// $(document).on('click', 'button:button', function(){
-//     title = $(this).val(); 
-$(document).on('click', 'input:button', function(){
-  title = $(this).val(); 
+    $('#submit').click(function () {
+        var userInput = $('#userInput').val().trim();
+        inputs.push(userInput);
+        addButtons();
+    })
+    // $(document).on('click', 'button:button', function(){
+    //     queue = $(this).val(); 
+    $(document).on('click', 'input:button', function () {
+        queue = $(this).val();
 
-  // queryURL for Giphy API
-  var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" + title +  "&api_key=kBZUhMtzJjJ4FUDxd9cqG0xgPhZ9nkZJ");
+        var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" + queue + "&api_key=kBZUhMtzJjJ4FUDxd9cqG0xgPhZ9nkZJ");
 
-    $.ajax({
-      url: queryURL,
-      method: 'GET'
-    }).done(function(response) {
-    console.log(response);
-    $(".buttons").empty();
-      for (var i = 0; i < 20; i++) {
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).done(function (response) {
+            console.log(response);
+            $(".buttons").empty();
+            for (var i = 0; i < 20; i++) {
 
-      var newRating =$("<div>");
-      var newGif = $("<img>");
-      var newDiv = $("<div>");
-      newDiv.addClass("giph");
-      newGif.addClass("gif");
-      newGif.attr("data-state", "animate");
-      newGif.attr("src", response.data[i].images.downsized.url);
-      newRating.html("Rating: " + response.data[i].rating);
-      $(newDiv).append(newRating);
-      $(newDiv).append(newGif);
-      $(".buttons").append(newDiv);
-      }
+                var newRating = $("<div>");
+                var newGif = $("<img>");
+                var newDiv = $("<div>");
+                newDiv.addClass("giph");
+                newGif.addClass("giphy");
+                newGif.attr("data-state", "still");
+                newGif.attr("data-still", response.data[i].images.downsized_still.url);
+                newGif.attr("data-animate", response.data[i].images.downsized.url);
+                newRating.html("Rating: " + response.data[i].rating);
+                newGif.attr("src", response.data[i].images.downsized.url);
+                $(newDiv).append(newRating);
+                $(newDiv).append(newGif);
+                $(".buttons").append(newDiv);
+            }
+
+        })
 
     })
+    $(document).on("click", ".giphy", function () {
+        state = $(this).attr("data-state");
+        var animated = $(this).attr("data-animate");
+        var still = $(this).attr("data-still");
 
-}) 
-          $(document).on("click", ".gif", function(){
-          var myvar = $(this).attr("id");
-          state = $(this).attr("data-state"); 
-          var animateURL = $(this).attr("data-animate");
-          var stillURL = $(this).attr("data-still");
+        if (state === "still") {
+            $(this).attr("data-state", "animate");
+            $(this).attr("src", animated);
+          } else{
+            $(this).attr("data-state", "still");
+            $(this).attr("src", still);
+          }
 
-            if (state === "animate") {
-              $(this).attr("data-state", "still");
-              $(this).attr("src", stillURL);
-            } else{
-              $(this).attr("data-state", "animate");
-              $(this).attr("src", animateURL);
-            }
-                        
-        })
+    })
 
 });
